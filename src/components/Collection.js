c283bea14d72ab939fdd1813de184a902fc83903
AlanSwenson <alan@alanswenson.dev>
2020-10-10 10:06:50 -0500

    basic collection display

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,21 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, InputGroup, FormGroup, TagInput } from "@blueprintjs/core";
 
 export function CollectionList() {
 
-    fetch('/list_recipes', {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-    }).then(data => {
-        console.log(data)
-    }
-    );
+    const [collectionList, setCollectionList] = useState([]);
+
+    useEffect(() => {
+        fetch('/list_recipes', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        })
+            .then(res => res.json())
+            .then(res => res.collection)
+            .then(res => {
+                console.log(res)
+                setCollectionList(res)
+            });
+    }, []);
 
     return (
         <div class="collection-container">
             <h1>Recipe Collection</h1>
-
-
+            {collectionList.map(item => (
+                <p>{item.title} - {item.link}
+                    <ul>
+                        {item.ingredients.map(ingredient => (
+                            <li>{ingredient}</li>
+                        ))}
+                    </ul>
+                </p>
+            ))
+            }
         </div>
     );
 
