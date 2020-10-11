9cbd0dbe02935fdc187b1dc1a565081395076142
AlanSwenson <alan@alanswenson.dev>
2020-10-10 19:27:11 -0500

    add hamburger

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Hamburger from 'hamburger-react'
 import { Link } from 'react-router-dom'
-import { Button, Navbar, Alignment } from "@blueprintjs/core";
+import { Button, Navbar, Alignment, Menu } from "@blueprintjs/core";
 import { InputGroup } from "@blueprintjs/core";
 
-class Nav extends React.Component {
-    render() {
-        return (
+export function Nav() {
+    const [isOpen, setOpen] = useState(false)
+
+    return (
+        <div>
+            <div class="d-flex justify-flex-end">
+                <Hamburger rounded toggled={isOpen} toggle={setOpen} />
+
+            </div>
+            { isOpen ?
+                <div class="d-flex justify-flex-end d-none-md">
+                    <Menu>
+                        <Menu.Item icon="shop" text="Restaurants" />
+                        <Menu.Item icon="book" text="Recipes" />
+                        <Menu.Item icon="plus" text="Add" />
+                    </Menu>
+
+                </div> : (
+                    null
+                )}
             <Navbar fixedToTop="true">
                 <Navbar.Group align={Alignment.RIGHT}>
                     <Navbar.Heading>Northeast Food Blog</Navbar.Heading>
@@ -20,10 +38,11 @@ class Nav extends React.Component {
                     <Link to="/add"><Button className="bp3-minimal" icon="plus" text="Add" /></Link>
                 </Navbar.Group>
             </Navbar >
-        );
+        </div >
+    );
 
-    }
 }
+
 
 export default Nav;
 
