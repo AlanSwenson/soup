19c3efe4a66d5fcb2088282834fbec41f85ed9bb
AlanSwenson <alan@alanswenson.dev>
2020-10-10 21:13:53 -0500

    better nav design

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,34 +10,40 @@ export function Nav() {
 
     return (
         <div>
-            <div class="d-flex justify-flex-end">
-                <Hamburger rounded toggled={isOpen} toggle={setOpen} />
 
-            </div>
-            { isOpen ?
-                <div class="d-flex justify-flex-end d-none-md">
-                    <Menu>
-                        <Link to="/restaurants" onClick={handleLink}><Menu.Item icon="shop" text="Restaurants" /></Link>
-                        <Link to="/recipes" onClick={handleLink}><Menu.Item icon="book" text="Recipes" /></Link>
-                        <Link to="/add" onClick={handleLink}><Menu.Item icon="plus" text="Add" /></Link>
-                    </Menu>
-
-                </div> : (
-                    null
-                )}
             <Navbar fixedToTop="true">
-                <Navbar.Group align={Alignment.RIGHT}>
+                <Navbar.Group align={Alignment.LEFT}>
                     <Navbar.Heading>Northeast Food Blog</Navbar.Heading>
-                    <Navbar.Divider />
-                    <InputGroup id="search"
-                        placeholder="Search"
-                        type="search"
-                        leftIcon="search"
-                    />
-                    <Link to="/restaurants"><Button className="bp3-minimal" icon="shop" text="Restaurants" /></Link>
-                    <Link to="/recipes"><Button className="bp3-minimal" icon="book" text="Recipes" /></Link>
-                    <Link to="/add"><Button className="bp3-minimal" icon="plus" text="Add" /></Link>
                 </Navbar.Group>
+
+                <div class="d-flex justify-flex-end">
+                    <Hamburger rounded toggled={isOpen} toggle={setOpen} />
+
+                </div>
+                {isOpen ?
+                    <Navbar.Group align={Alignment.RIGHT}>
+                        <div class="d-flex justify-flex-end d-none-md">
+                            <Menu>
+                                <Link to="/restaurants" onClick={handleLink}><Menu.Item icon="shop" text="Restaurants" /></Link>
+                                <Link to="/recipes" onClick={handleLink}><Menu.Item icon="book" text="Recipes" /></Link>
+                                <Link to="/add" onClick={handleLink}><Menu.Item icon="plus" text="Add" /></Link>
+                            </Menu>
+
+                        </div>
+                    </Navbar.Group> : (
+                        <Navbar.Group align={Alignment.RIGHT} className="sm-menu">
+                            <InputGroup id="search"
+                                placeholder="Search"
+                                type="search"
+                                leftIcon="search"
+                            />
+                            <Link to="/restaurants"><Button className="bp3-minimal" icon="shop" text="Restaurants" /></Link>
+                            <Link to="/recipes"><Button className="bp3-minimal" icon="book" text="Recipes" /></Link>
+                            <Link to="/add"><Button className="bp3-minimal" icon="plus" text="Add" /></Link>
+                        </Navbar.Group>
+                    )}
+
+
             </Navbar >
         </div >
     );
